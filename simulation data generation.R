8597949e3d7e3688261f300493970ae776f48c3a
nirkeret <50802985+nirkeret@users.noreply.github.com>
2024-10-17 17:25:50 -0700

    Update simulation data generation.R

--- simulation data generation.R
+++ simulation data generation.R
@@ -32,15 +32,12 @@ i = 1
 while(i <= N)
 {
   set.seed(seedBase + i)
+  #nn is the number of observations generated, out of which we sample n observations that satisfy death > recruitment (T2>R).   
   nn = 5000 # if n = 1500
   # nn = 50000 # if n = 10000
-  
-  # ## settings B and C
-  # rho = 0.8
-  # S = matrix(rho,8,8)
-  # diag(S) = 1
-  # X = pnorm(mvrnorm(nn,mu=rep(0,8),Sigma = S))
 
+  ## generating covariates:
+  
   ## setting A:
   X = matrix(NA,nn,8)
   X[,1] = rgamma(nn,2,6) 
@@ -52,7 +49,15 @@ while(i <= N)
   X[,7] = rpois(nn,5)
   X[,8] = runif(nn)
   X = apply(X,2,UnitRescale)
+  
+  # ## settings B and C
+  # rho = 0.8
+  # S = matrix(rho,8,8)
+  # diag(S) = 1
+  # X = pnorm(mvrnorm(nn,mu=rep(0,8),Sigma = S))
 
+  ## sampling disease and death-before-disease times:
+  
   T1 = rexp(nn,H_const_dis * exp(X %*% b_dis))
   
   T2 = rexp(nn,H_const_dth1 * exp(X[,b_dth1_ind] %*% b_dth1)) #settings A and B
@@ -60,12 +65,15 @@ while(i <= N)
   #setting C:
   # mu = sin(X[,b_dth1_ind[1]] * pi) + 2 * abs(X[,b_dth1_ind[2]] - 0.5) + X[,b_dth1_ind[3]]^3
   # T2 = rexp(nn,0.04 / mu)
-  
+
+  ## sampling censoring times:
   C = rexp(nn,H_const_cens) # setting A
   # C = rexp(nn,H_const_cens * exp(X[,b_cens_ind] %*% b_cens)) #setting B
   # C = rlnorm(nn,meanlog = 3*abs(X[,b_cens_ind[1]]-0.5) + 2*X[b_cens_ind[2]], sdlog = 1.5) #setting C
   
-  ## sampling recruitment age, setting A:
+  ## sampling recruitment age:
+  
+  #setting A:
   u = runif(nn)
   a = 4; b = 22; c = 13
   R = ifelse(u>0.5,b - sqrt((1-u)*(b-a)*(b-c)),a + sqrt(u * (b-a)*(c-a)))
@@ -79,11 +87,13 @@ while(i <= N)
   D2 = V == T2
   n_dis = sum(D1)
   
-  # sampling death after illness, settings A and B:
+  ## sampling death after disease:
+
+  #settings A and B:
   rates = as.vector(H_const_dth2 * exp(cbind(X[,b_dth2_ind],V)[D1,] %*% b_dth2))
   T2[D1] = rexp(n_dis,rates) + V[D1]  
   
-  # sampling death after illness, setting C:
+  #setting C:
   # mu = 0.5 + (cos(pi*X[,b_dth2_ind[1]])^2 + 2*abs(X[,b_dth2_ind[2]]-0.5))[D1] + sqrt(V[D1])/3
   # T2[D1] = rgamma(n_dis,scale = 3, shape = mu) + V[D1]  
 
@@ -91,7 +101,8 @@ while(i <= N)
   W = pmin(T2, R + C)
   obs = which(T2 > R)
   samp = sample(obs,n)  # sample n observations out of nn, satisfying T2>R.
-  
+
+  ## keep the n sampled observations
   V = V[samp]
   R = R[samp]
   X = X[samp,]
