436fc63cf82f90150735df98a3737c772d262e5e
nirkeret <50802985+nirkeret@users.noreply.github.com>
2024-10-21 17:22:51 -0700

    Update Estimation Procedure.R

--- Estimation Procedure.R
+++ Estimation Procedure.R
@@ -17,19 +17,21 @@ n = nrow(X) #sample size
 numPairs = 25 # user-defined number of sampled pairs per observation. 
 
 #fitting marginal Cox models using partial likelihood 
-  
+suppressWarnings({    #warnings are induced when inserting prevalent observations into the coxph function
 fit1 = coxph(Surv(R, V, D1, type = "counting") ~ X)
 fit2 = coxph(Surv(R, V, D2, type="counting") ~ X)
 # V or functional forms of it can be incorporated as covariates in the illness -> death transition:
 fit3 = coxph(Surv(pmax(R,V), W, D3, type = "counting") ~ X + V, subset = D1)
 # If censoring is assumed random and should be estimated too:
 fitC = coxph(Surv(R,V,1-D1-D2,type = "counting") ~ X)
-
+})
 #Getting the estimated baseline hazard functions:
+suppressWarnings({  
 H012_estimated = stepfun(basehaz(fit1,centered = F)[,2],c(0,basehaz(fit1,centered = F)[,1]))
 H013_estimated = stepfun(basehaz(fit2,centered = F)[,2],c(0,basehaz(fit2,centered = F)[,1]))
 H023_estimated = stepfun(basehaz(fit3,centered = F)[,2],c(0,basehaz(fit3,centered = F)[,1]))
 H0C_estimated = stepfun(basehaz(fitC,centered = F)[,2],c(0,basehaz(fitC,centered = F)[,1]))
+})
   
 elp13 = exp(X %*% coef(fit2))
 elp23 = exp(cbind(X,V) %*% coef(fit3))
